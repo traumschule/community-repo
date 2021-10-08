45882c62064f3c88d19b3a1cb5199adbc443e96e
Joystream Stats <dev@joystreamstats.live>
2021-10-08 23:26:30 +0000

    update config, hidden threads

--- community-contributions/joystreamtelegrambot/config.ts
+++ community-contributions/joystreamtelegrambot/config.ts
@@ -1,17 +1,19 @@
 // website
-export const domain = "https://testnet.joystream.org";
+export const domain = "https://pioneer.joystreamstats.live";
 
 // websocket location
-export const wsLocation = "wss://rome-rpc-endpoint.joystream.org:9944/";
+export const wsLocation = "wss://joystreamstats.live:9945";
 
-// telegram bot token
-export const token: string = "1168139699:AAHUWqHWqteAy55yM2yhF1XMvV1BgmYvfog";
+export const discordToken = ""
+export const tgToken = "";
 
 // telegram chat ID
-export const chatid: string = "-1001438587296";
+export const chatid = "-1001438587296";
 
 // time between heartbeat announcement in milliseconds
 export const heartbeat = 60000 * 60 * 6;
 
 // minutes between checking for proposal updates
 export const proposalDelay = 15
+
+export const suppressedThreads = [180, 265, 275, 390]; // 180 tokens, 265 faucet, 275 pets, 390 bounty-24
